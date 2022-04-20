f87100944392c93ccafdc05a7c5ba6a6f4aa3ce6
七画一只妖 <1157529280@qq.com>
2022-04-20 20:57:08 +0800

    新增：花海网关

---
+++ gateway/src/main/java/top/twip/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package top.twip.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 七画一只妖
+ * @Date: 2022-04-20 20:24
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
